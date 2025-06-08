c3d2afd0ed10a9269f5997599a350055299385c7
João Vitor L. de Oliveira <j.liberato@vertex.vertexdigital.co>
2025-06-08 10:47:01 -0300

    Adicionado registro que usa userRepository para adicionar a dependencia antes de gerar o token

--- src/Shared/Containers/Middlewares/AutenticationHashRoutesContainer.shared.container.middleware.ts
+++ src/Shared/Containers/Middlewares/AutenticationHashRoutesContainer.shared.container.middleware.ts
@@ -2,7 +2,14 @@ import { container } from "tsyringe"
 import { UserService } from "../../../Application/Services/UserService.application.service"
 import { InternalNotificationServiceAdapter } from "../../../Infrastructure/Adapters/Internal/InternalNotificationAdapter.infrastructure.adapters"
 import { UserRepository } from "../../../Infrastructure/Repositories/User.infrastructure.repositories"
+import { IUserRepository, UserUseCase } from "../../../Domain/Usecases/UserUseCase.domain.usecases.user"
+
+container.register<IUserRepository>(
+  "IUserRepository",
+  { useClass: UserRepository }
+)
 
 container.registerSingleton<UserRepository>(UserRepository)
 container.registerSingleton<InternalNotificationServiceAdapter>(InternalNotificationServiceAdapter)
+container.registerSingleton<UserUseCase>(UserUseCase)
 container.registerSingleton<UserService>(UserService)
