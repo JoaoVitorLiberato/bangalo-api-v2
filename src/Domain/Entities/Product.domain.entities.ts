78b15349ac1bf1da2a3a7f07f0b56933e8e74135
João Vitor L. de Oliveira <j.liberato@vertex.vertexdigital.co>
2025-06-07 21:56:46 -0300

    Arquivo productFactory criado e alterando model product.

--- src/Domain/Entities/Product.domain.entities.ts
+++ src/Domain/Entities/Product.domain.entities.ts
@@ -2,43 +2,36 @@ import { Complement } from "./Complement.domain.entities"
 
 export class Product {
   constructor (
-    public url_image: string,
-    public  categoryId: string,
+    public tumbnail: {
+      url: string,
+      upload: boolean,
+      location: string,
+    },
     public name: string,
     public  description: string,
-    public price: {
-      default: number,
-      qtd_product?: number,
-      total?: number,
-      total_price_complements?: number
-      discount: {
-        active: boolean,
-        value: number
-      },
-    },
-    public complements: Complement[],
+    public price: number,
     public differences: {
       especial: {
         readonly: boolean,
         input: string,
         active: boolean,
-        additional: number
+        value: number
       },
       breaded: {
         readonly: boolean
         input: string,
         active: boolean,
-        additional: number
+        value: number
       },
       flambed: {
         readonly: boolean
         input: string,
         active: boolean,
-        additional: number
+        value: number
       }
     },
+    public  categoryId: string,
     public note_client: number,
-    public upload: boolean,
     public id?: string,
   ) {}
 }
---
+++ src/Domain/Factory/ProductFactory.domain.factory.ts
@@ -0,0 +1,64 @@
+import { Product } from "../Entities/Product.domain.entities"
+import { Name } from "../ValueObjects/Name.domain.valuesobjects"
+import { Thumbnail } from "../ValueObjects/Tumbnail.domain.valuesobjects"
+import { Description } from "../ValueObjects/Description.domain.valuesobjects"
+import { Price } from "../ValueObjects/Price.domain.valuesobjects"
+
+interface ProductFactoryProps {
+  tumbnail: {
+    url: string,
+    upload: boolean,
+    location: "products",
+  },
+  name: string,
+  description: string,
+  price: number,
+  differences: {
+    especial: {
+      readonly: boolean,
+      input: string,
+      active: boolean,
+      value: number
+    },
+    breaded: {
+      readonly: boolean,
+      input: string,
+      active: boolean,
+      value: number
+    },
+    flambed: {
+      readonly: boolean,
+      input: string,
+      active: boolean,
+      value: number
+    }
+  },
+  note_client: number,
+  category_id: string,
+}
+
+export class ProductFactory {
+  static save (data: ProductFactoryProps): Product {
+    const TUMBNAIL = new Thumbnail(data.tumbnail.url, data.tumbnail.location);
+    const NAME = new Name(data.name);
+    const DESCRIPTION = new Description(data.description);
+    const PRICE = new Price(data.price);
+    const DIFFERENCES = data.differences;
+    const NOTE_CLIENT = data.note_client;
+    const CATEGORY_ID = data.category_id;
+
+    return new Product(
+      {
+        url: TUMBNAIL.url_image,
+        location: TUMBNAIL.location,
+        upload: data.tumbnail.upload,
+      },
+      NAME.getValue(), 
+      DESCRIPTION.getValue(),
+      PRICE.getValue(),
+      DIFFERENCES,
+      CATEGORY_ID,
+      NOTE_CLIENT,
+    );
+  }
+}
